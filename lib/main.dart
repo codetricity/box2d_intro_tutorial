18b0b6f2510fe314c50fd6d98fbbbcb44af23577
Craig <craig@oppkey.com>
2022-07-02 14:36:23 -0700

    added crates and barrels

--- lib/main.dart
+++ lib/main.dart
@@ -21,13 +21,13 @@ class MyGame extends Forge2DGame {
     add(Ground(gameSize));
     add(
       Player(
-        position: Vector2(20, 5),
+        position: Vector2(40, 5),
         sprite: await loadSprite('natsuki_casual.webp'),
       ),
     );
     add(
       Player(
-        position: Vector2(30, 10),
+        position: Vector2(60, 10),
         sprite: await loadSprite('monika.webp'),
       ),
     );
@@ -39,10 +39,15 @@ class MyGame extends Forge2DGame {
     );
     add(
       Player(
-        position: Vector2(20, 30),
+        position: Vector2(50, 30),
         sprite: await loadSprite('yuri_casual.webp'),
       ),
     );
+    add(GroundObstacle(Vector2(100, 36)));
+    add(GroundObstacle(Vector2(5, 50)));
+    add(Obstacle(Vector2(100, 28)));
+    add(Obstacle(Vector2(100, 20)));
+    add(Obstacle(Vector2(100, 12)));
   }
 }
 
@@ -83,9 +88,87 @@ class Ground extends BodyComponent {
   @override
   Body createBody() {
     final shape = EdgeShape()
-      ..set(Vector2(0, gameSize.y - 5), Vector2(gameSize.x, gameSize.y));
+      ..set(Vector2(0, gameSize.y - 4), Vector2(gameSize.x, gameSize.y));
     final fixtureDef = FixtureDef(shape, friction: 0.3);
     final bodyDef = BodyDef(userData: this, position: Vector2.zero());
+    return world.createBody(bodyDef)..createFixture(fixtureDef);
+  }
+}
+
+class GroundObstacle extends BodyComponent {
+  final Vector2 position;
+
+  GroundObstacle(this.position);
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    renderBody = false;
+    add(
+      SpriteComponent(
+        sprite: await gameRef.loadSprite('barrel.png'),
+        size: Vector2.all(6),
+        anchor: Anchor.center,
+      ),
+    );
+  }
+
+  @override
+  Body createBody() {
+    final shape = PolygonShape();
+    final vertices = [
+      Vector2(-3, -3),
+      Vector2(3, -3),
+      Vector2(-3, 2.8),
+      Vector2(3, 3)
+    ];
+    shape.set(vertices);
+    final fixtureDef = FixtureDef(
+      shape,
+      friction: 0.3,
+      density: 1,
+    );
+    final bodyDef =
+        BodyDef(userData: this, position: position, type: BodyType.dynamic);
+    return world.createBody(bodyDef)..createFixture(fixtureDef);
+  }
+}
+
+class Obstacle extends BodyComponent {
+  final Vector2 position;
+
+  Obstacle(this.position);
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    renderBody = false;
+    add(
+      SpriteComponent(
+        sprite: await gameRef.loadSprite('crate.png'),
+        size: Vector2.all(6),
+        anchor: Anchor.center,
+      ),
+    );
+  }
+
+  @override
+  Body createBody() {
+    final shape = PolygonShape();
+    final vertices = [
+      Vector2(-3, -3),
+      Vector2(3, -3),
+      Vector2(-3, 3),
+      Vector2(3, 3)
+    ];
+    shape.set(vertices);
+    final fixtureDef = FixtureDef(
+      shape,
+      friction: 0.3,
+      density: 1,
+    );
+    final bodyDef =
+        BodyDef(userData: this, position: position, type: BodyType.dynamic);
     return world.createBody(bodyDef)..createFixture(fixtureDef);
   }
 }
