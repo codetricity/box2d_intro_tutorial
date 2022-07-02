6d9f8fb62ca132587be24b9b45ddbd184d469058
Craig <craig@oppkey.com>
2022-07-01 19:54:42 -0700

    ground and sprite connected to BodyComponent

--- README.md
+++ README.md
@@ -1,16 +1,7 @@
-# box2d_demo
+# Flutter Box2D Game Tutorial with Flame and Forge2D
 
-A new Flutter project.
+![screenshot](docs/screenshot.gif)
 
-## Getting Started
+## Video Tutorials
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+1. [Box2D Game Physics for Flutter](https://youtu.be/EzyJd-DvW58)
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/material.dart';
@@ -11,19 +12,80 @@ void main() {
 }
 
 class MyGame extends Forge2DGame {
+  MyGame() : super(gravity: Vector2(0, 7.0));
   @override
   Future<void> onLoad() async {
     await super.onLoad();
-    add(Player());
+    Vector2 gameSize = screenToWorld(camera.viewport.effectiveSize);
+    print(gameSize);
+    add(Ground(gameSize));
+    add(
+      Player(
+        position: Vector2(20, 5),
+        sprite: await loadSprite('natsuki_casual.webp'),
+      ),
+    );
+    add(
+      Player(
+        position: Vector2(30, 10),
+        sprite: await loadSprite('monika.webp'),
+      ),
+    );
+    add(
+      Player(
+        position: Vector2(30, 35),
+        sprite: await loadSprite('kieran.webp'),
+      ),
+    );
+    add(
+      Player(
+        position: Vector2(20, 30),
+        sprite: await loadSprite('yuri_casual.webp'),
+      ),
+    );
   }
 }
 
 class Player extends BodyComponent {
+  final Vector2 position;
+  final Sprite sprite;
+
+  Player({required this.position, required this.sprite});
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    renderBody = false;
+    add(
+      SpriteComponent(
+        sprite: sprite,
+        size: Vector2.all(12),
+        anchor: Anchor.center,
+      ),
+    );
+  }
+
   @override
   Body createBody() {
     final shape = CircleShape()..radius = 6;
-    final fixtureDef = FixtureDef(shape, friction: 0.5);
-    final bodyDef = BodyDef(position: Vector2(20, 5), type: BodyType.dynamic);
+    final fixtureDef =
+        FixtureDef(shape, density: 1.0, restitution: .4, friction: 0.5);
+    final bodyDef = BodyDef(position: position, type: BodyType.dynamic);
+    return world.createBody(bodyDef)..createFixture(fixtureDef);
+  }
+}
+
+class Ground extends BodyComponent {
+  final Vector2 gameSize;
+
+  Ground(this.gameSize);
+
+  @override
+  Body createBody() {
+    final shape = EdgeShape()
+      ..set(Vector2(0, gameSize.y - 3), Vector2(gameSize.x, gameSize.y - 3));
+    final fixtureDef = FixtureDef(shape, friction: 0.3);
+    final bodyDef = BodyDef(userData: this, position: Vector2.zero());
     return world.createBody(bodyDef)..createFixture(fixtureDef);
   }
 }
